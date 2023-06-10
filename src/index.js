08f9617d450d9b4f9479d8e203c16d338ddc6320
parteek kumar <digitalparteek@gmail.com>
2023-06-10 14:27:55 +0530

    Added Notfication ticket model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const bodyParser = require("body-parser");
+const cron = require("node-cron");
 
 const sendBasicEmail = require("./services/email-service");
 
@@ -13,12 +14,16 @@ const setupAndStartServer = () => {
   app.listen(PORT, () => {
     console.log(`Server started on PORT: ${PORT}`);
 
-    sendBasicEmail(
-      "support@admin.com",
-      "sarcaxx9999@gmail.com",
-      "This is a testing Email",
-      "Hey, how are you, I hope you like the support"
-    );
+    // sendBasicEmail(
+    //   "support@admin.com",
+    //   "sarcaxx9999@gmail.com",
+    //   "This is a testing Email",
+    //   "Hey, how are you, I hope you like the support"
+    // );
+
+    // cron.schedule("*/2 * * * *", () => {
+    //   console.log("running a task every two minutes");
+    // });
   });
 };
 
